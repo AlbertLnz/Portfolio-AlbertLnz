1cde4388f6aaed59b94ddf6060a0652d45c566dd
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-23 16:41:19 +0100

    Fix errors & favicon changed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import vercelServerless from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,4 +14,6 @@ export default defineConfig({
       redirectToDefaultLocale: false
     }
   },
+  output: 'server',
+  adapter: vercelServerless(),
 });
--- src/components/JobsComponent.jsx
+++ src/components/JobsComponent.jsx
@@ -12,7 +12,7 @@ const JobsComponent = ({ currentLocale, id }) => {
             <li className="text-xs">{jobData.period}</li>
         </ul>
 
-        <hr text={i18n.JOBS.SEPARATOR} className="overflow-visible p-0 border-t-2 w-[90%] border-double dark:border-white text-center after:content-[attr(text)] after:inline-block after:relative after:top-[-1em] after:text-sm after:px-1 after:bg-[#d1d5db] after:dark:bg-[#171717]"/>
+        <hr text={i18n.JOBS.SEPARATOR} className="overflow-visible p-0 border-t-2 w-[90%] border-double border-black dark:border-white text-center after:content-[attr(text)] after:inline-block after:relative after:top-[-1em] after:text-sm after:px-1 after:bg-[#d1d5db] after:dark:bg-[#171717]"/>
 
         <div className="flex flex-wrap gap-x-2 text-sm mt-2 justify-center">
             {jobData.abilities.map((ability) => (
