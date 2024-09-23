41c8efc25bfd523f61763748a474fd611b3c9793
Albert Lanza <albert2000_lanza@hotmail.com>
2024-09-23 10:03:57 +0200

    Work section implemented

---
+++ src/data/works.js
@@ -0,0 +1,20 @@
+export const workItems = [
+  {
+    company: 'Get in touch',
+    title: 'Current available to work',
+    period: 'Oct 24 - Now',
+    link: 'https://www.linkedin.com/in/albert-lanza-rio/',
+  },
+  {
+    company: 'No Country',
+    title: 'PHP Backend Developer',
+    period: 'Nov 23 - May 24',
+    link: 'https://www.nocountry.tech/',
+  },
+  {
+    company: 'IT Academy',
+    title: 'React Frontend Developer',
+    period: 'Sept 22 - Nov 23',
+    link: 'https://www.barcelonactiva.cat/itacademy',
+  },
+]
