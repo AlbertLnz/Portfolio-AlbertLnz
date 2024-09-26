b901aa488a2ab8997b30649a5af385f8a2de52cc
Albert Lanza <albert2000_lanza@hotmail.com>
2024-09-26 20:01:03 +0200

    More styling + Fix anchor of blog posts (always available)

--- src/data/blogCardAssignColor.js
+++ src/data/blogCardAssignColor.js
@@ -1,15 +1,18 @@
 export const blogCardAssignColor = {
   Tutorial: {
+    background: 'bg-[#1F4013]',
     backgroundPosts: 'to-[#1F4013]',
     backgroundPost: 'from-[#1F4013]',
     border: 'border-[#2C2C2C]',
   },
   Snippet: {
+    background: 'bg-[#0A344A]',
     backgroundPosts: 'to-[#0A344A]',
     backgroundPost: 'from-[#0A344A]',
     border: 'border-[#2C2C2C]',
   },
   Theme: {
+    background: 'bg-[#4B3D04]',
     backgroundPosts: 'to-[#4B3D04]',
     backgroundPost: 'from-[#4B3D04]',
     border: 'border-[#2C2C2C]',
