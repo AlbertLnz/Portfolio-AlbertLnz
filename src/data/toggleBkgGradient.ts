6850756413c41c977655dfe787335a259e6dbc8a
Albert Lanza <albert2000_lanza@hotmail.com>
2024-10-17 10:30:24 +0200

    Bkg gradient also in single Blog post

--- src/data/toggleBkgGradient.ts
+++ src/data/toggleBkgGradient.ts
@@ -1,23 +1,48 @@
-export const toggleBkgGradient = (elts: string, object: any) => {
+export const toggleBkgGradient = (
+  domElement: 'class' | 'id',
+  elts: string,
+  object: any,
+  gradients: object
+) => {
   const applyTheme = () => {
-    const domElts = document.querySelectorAll(`.${elts}`)
+    let domElts
+    if (domElement === 'class') {
+      domElts = document.querySelectorAll(`.${elts}`)
+    }
 
-    domElts.forEach((domElt) => {
+    if (domElement === 'id') {
+      domElts = document.querySelectorAll(`#${elts}`)
+    }
+
+    domElts?.forEach((domElt) => {
       const tag = domElt.getAttribute('data-tag')
       const clearedTag = tag?.replace('post-', '')
 
       if (clearedTag && object) {
-        const { border, backgroundPosts } = object[clearedTag]
+        const { border, backgroundPosts, backgroundPost } = object[clearedTag]
+        const theme = localStorage.getItem('theme')
 
-        const isDark = `bg-gradient-to-b from-[#171717] via-30% via-[#171717] to-100% ${backgroundPosts} border ${border}`
-        const isLight = `bg-gradient-to-b from-[#EBEBF2] via-30% via-[#f2f2f2] to-100% ${backgroundPosts} border ${border}`
+        const gradientsMap = {
+          class: {
+            dark: `${gradients.dark} ${backgroundPosts} ${border}`,
+            light: `${gradients.light} ${backgroundPosts} ${border}`,
+          },
+          id: {
+            dark: `${gradients.dark} ${backgroundPost} ${border}`,
+            light: `${gradients.light} ${backgroundPost} ${border}`,
+          },
+        }
 
-        domElt.classList.remove(...isDark.split(' '), ...isLight.split(' '))
+        const currentGradient = gradientsMap[domElement]
+        if (currentGradient) {
+          domElt.classList.remove(
+            ...currentGradient.dark.split(' '),
+            ...currentGradient.light.split(' ')
+          )
 
-        if (localStorage.getItem('theme') === 'dark') {
-          domElt.classList.add(...isDark.split(' '))
-        } else if (localStorage.getItem('theme') === 'light') {
-          domElt.classList.add(...isLight.split(' '))
+          const selectedGradient =
+            theme === 'dark' ? currentGradient.dark : currentGradient.light
+          domElt.classList.add(...selectedGradient.split(' '))
         }
       }
     })
@@ -31,7 +56,7 @@ export const toggleBkgGradient = (elts: string, object: any) => {
       if (storedTheme !== null) {
         applyTheme()
       }
-    }, 1)
+    }, 100)
 
     window.addEventListener('beforeunload', () => {
       clearInterval(interval)
