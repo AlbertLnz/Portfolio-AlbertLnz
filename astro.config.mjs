5f380c884088e89a53571b5ebe3f20771669e414
Albert Lanza <albert2000_lanza@hotmail.com>
2025-04-01 09:43:34 +0200

    New portofolio version w/ Astro 5.5.6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,44 +1,5 @@
 // @ts-check
-import { defineConfig, envField, passthroughImageService } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/serverless'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind()],
-
-  image: { service: passthroughImageService() },
-
-  redirects: {
-    '/': 'en',
-  },
-
-  experimental: {
-    env: {
-      schema: {
-        PRODUCTION: envField.boolean({
-          context: 'server',
-          access: 'public',
-        }),
-        GITHUB_API_BASE_URL: envField.string({
-          context: 'server',
-          access: 'public',
-        }),
-        GITHUB_README_API: envField.string({
-          context: 'server',
-          access: 'public',
-        }),
-        OPEN_WEATHER_API_URL: envField.string({
-          context: 'server',
-          access: 'public',
-        }),
-        OPEN_WEATHER_API_KEY: envField.string({
-          context: 'server',
-          access: 'secret',
-        }),
-      },
-    },
-  },
-  output: 'server',
-  adapter: vercel(),
-})
+export default defineConfig({})
