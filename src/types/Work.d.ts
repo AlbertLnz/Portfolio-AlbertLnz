1cc5ab98fcea52c003a9e42180a0e4a0f186b5db
Albert Lanza <albert2000_lanza@hotmail.com>
2025-04-01 13:57:10 +0200

    Work cards added (Works, WorkItem & ListItemBadge) w/ TS interface

---
+++ src/types/Work.d.ts
@@ -0,0 +1,9 @@
+export interface IWork {
+  title: string
+  paragraph: string
+  skills: string[]
+  skills_urls: Array<`https://${string}` | ''>
+  company: string
+  company_url: string
+  period: string
+}
